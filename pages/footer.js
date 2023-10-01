f03743be4b82b19f4070387ecd8147488e457e67
NCC-Vrsec <146008799+NCC-Vrsec@users.noreply.github.com>
2023-10-01 19:25:32 +0530

    Add files via upload

---
+++ pages/footer.js
@@ -0,0 +1,10 @@
+var typed3 = new Typed('#typed3', {
+    strings: ['for VRSEC NCC'],
+    typeSpeed: 90,
+    backSpeed: 0,
+    // backDelay: 500,
+    // startDelay: 1000,
+    smartBackspace: true, // this is a default
+    loop: true,
+    showCursor: false
+});
